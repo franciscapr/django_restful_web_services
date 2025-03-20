0c6e756f1fa477b40719cfa96a86e15f170d3958
Francisca <francisca.paredes.pr@gmail.com>
2025-03-20 14:28:29 -0300

    config apps in settings.py

--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Django REST framework
+    'rest_framework',
+
+    # Toys applicarions
+    'toys.apps.ToysConfig',
+
 ]
 
 MIDDLEWARE = [
