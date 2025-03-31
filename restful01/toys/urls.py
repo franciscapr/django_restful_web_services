7f3079cac2c0570f928e9eee971387f517cab6aa
Francisca <francisca.paredes.pr@gmail.com>
2025-03-31 11:13:18 -0300

    create database

--- restful01/toys/urls.py
+++ restful01/toys/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, re_path, include
 from toys import views
 
 urlpatterns = [
-    path('toys/', views.toy_list),
-    re_path(r'^toys/(?P<pk>[0-9]+)$', views.toy_detail),    # re_path es para el regex
+    path(r'^', include('drones.urls')),
 ]
