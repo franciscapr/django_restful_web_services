c26368f04d78ad7b84b6abd41d83dd3e50d4bcf5
Francisca <francisca.paredes.pr@gmail.com>
2025-03-31 11:51:24 -0300

    serializers.py

---
+++ restful01/drones/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from drones.models import DroneCategory
+from drones.models import Drone
+from drones.models import Pilot
+from drones.models import Competition
+import drones.views
+
+class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
+    drones = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='drone-detail'
+    )
+
+    class Meta:
+        model = DroneCategory
+        fields = (
+            'url',
+            'pk',
+            'name',
+            'drones'
+        )
