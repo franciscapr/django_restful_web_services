1f85c1770675939564336fe0b1c1d56bb4e5905a
Francisca <francisca.paredes.pr@gmail.com>
2025-04-01 09:04:07 -0300

    DroneCategoryList

--- restful01/drones/views.py
+++ restful01/drones/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import reverse
+from drones.models import DroneCategory
+from drones.models import Drone
+from drones.models import Pilot
+from drones.models import Competition
+from drones.serializers import DroneCategorySerializer
+from drones.serializers import DroneSerializer
+from drones.serializers import PilotSerializer
+from drones.serializers import PilotCompetitionSerializer
 
-# Create your views here.
+
+class DroneCategoryList(generics.ListCreateAPIView):
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-list'
+
