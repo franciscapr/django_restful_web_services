21e1ed56c6ad423e323638642dae4629ef999501
Francisca <francisca.paredes.pr@gmail.com>
2025-04-02 10:29:36 -0300

    ordering_fields

--- restful01/drones/views.py
+++ restful01/drones/views.py
@@ -108,6 +108,13 @@ class CompetitionList(generics.ListCreateAPIView):
     queryset = Competition.objects.all()
     serializer_class = PilotCompetitionSerializer
     name = 'competition-list'
+    filter_class = CompetitionFilter
+    ordering_fields = (
+        'distance_in_feet',
+        'distance_achievement_date',
+    )
+
+
 
 class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Competition.objects.all()
