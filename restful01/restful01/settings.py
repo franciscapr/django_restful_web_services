ecc08a33199962357fed1f76067e9a224cc77c6c
Francisca <francisca.paredes.pr@gmail.com>
2025-04-02 09:19:53 -0300

    drones.custompagination settings.py

--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -145,6 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
-    'rest_framework.pagination.LimitOffsetPagination',
+    'drones.custompagination.LimitOffsetPaginationWithUpperBound',
     'PAGE_SIZE': 4
 }
