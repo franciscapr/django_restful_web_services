160e5cd4fb5e3a81af9c6123638ce3592916e806
Francisca <francisca.paredes.pr@gmail.com>
2025-04-03 09:06:58 -0300

    auth

--- restful01/drones/serializers.py
+++ restful01/drones/serializers.py
@@ -4,6 +4,31 @@ from drones.models import Drone
 from drones.models import Pilot
 from drones.models import Competition
 import drones.views
+from django.contrib.auth.models import User
+
+
+class UserDroneSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Drone
+        fields = (
+            'url',
+            'name'
+        )
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    drones = UserDroneSerializer(
+        many=True,
+        read_only=True
+    )
+    class Meta:
+        model = User
+        fields = (
+            'url',
+            'pk',
+            'username',
+            'drone'
+        )
+
 
 class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
     drones = serializers.HyperlinkedRelatedField(
--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -153,4 +153,8 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthenticaction',
+    )
 }
