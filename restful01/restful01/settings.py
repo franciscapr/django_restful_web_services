32f4111d8ef75f8cd5443a2f4eb22c62e95d079e
Francisca <francisca.paredes.pr@gmail.com>
2025-04-03 09:40:52 -0300

    migration for owner

--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -155,6 +155,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthenticaction',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
